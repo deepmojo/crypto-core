7759a981f1fec953dacb5a7c3b914dd9f1a968d2
GitHub Action <action@github.com>
2024-10-25 03:45:45 +0000

    Sync from monorepo - v1.0.0

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   verbose: false,
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json'
+    }
+  },
   collectCoverage: false,
   coverageReporters: [],
   coverageDirectory: 'coverage',
-  testEnvironment: "jsdom",
   testPathIgnorePatterns: [
     'lib/'
   ]
